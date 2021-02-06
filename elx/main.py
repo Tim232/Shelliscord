b0169be0d292c800f0694788377929dce44529f5
Shio <shio7113@gmail.com>
2021-02-06 20:23:36 +0900

    x

--- elx/main.py
+++ elx/main.py
@@ -1,2 +1,7 @@
-print("Your Discord account's E-mail>>")
+print("Your Discord account's E-mail >>")
 email = input()
+print("Your Discord account's Password >>")
+password = input()
+
+def login(email, password):
+    print("wut")
