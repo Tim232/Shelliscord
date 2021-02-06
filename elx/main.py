2824bf31c8b8eb03fdbb14df38f5808b73ebd6e2
Shio <shio7113@gmail.com>
2021-02-06 21:00:07 +0900

    Update main.py

--- elx/main.py
+++ elx/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+import os
 
 def login(email, password):
     driver = webdriver.Chrome("../ChromeDriver/chromedriver.exe")
